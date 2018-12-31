48c138567d906581fc756886510fb2dd77960ae9
zachalexander <alexander.d.zachary@gmail.com>
2018-12-31 14:56:19 -0500

    adds senate service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SenateVotesComponent } from './senate-votes/senate-votes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SenateVotesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/senate-votes/senate-votes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-senate-votes',
+  templateUrl: './senate-votes.component.html',
+  styleUrls: ['./senate-votes.component.scss']
+})
+export class SenateVotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
