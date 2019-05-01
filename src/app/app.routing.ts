1add1704c6e5fd301b625bd815e015ab9adf167d
zachalexander <alexander.d.zachary@gmail.com>
2019-05-01 16:42:57 -0400

    fixes removal of map again

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,14 +8,14 @@ import { HttpModule } from '@angular/http';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SenateVotesComponent } from './senate-votes/senate-votes.component'
-import { UsMapComponent } from './us-map/us-map.component'
+// import { UsMapComponent } from './us-map/us-map.component'
 
 
 const routes: Routes = [
     { path: '',             component: HomeComponent },
     { path: 'user-profile',     component: ProfileComponent },
     { path: 'senate-votes',     component: SenateVotesComponent },
-    { path: 'us-map',     component: UsMapComponent },
+    // { path: 'us-map',     component: UsMapComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
